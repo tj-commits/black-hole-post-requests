bff40a8e591633fdaf17670e6d4aba98accdadfb
tj-commits <thefoxjones@gmail.com>
2024-06-19 16:52:16 -0500

    add propfind and proppatch methods

--- server.js
+++ server.js
@@ -96,6 +96,13 @@ app.unsubscribe('*', (req, res) => {
   res.sendStatus(200)
 })
 
+app.propfind('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.proppatch('*', (req, res) => {
+  res.sendStatus(200)
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Listening on port ' + (process.env.PORT || 8080))
