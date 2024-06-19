4854cb857662369c83d2cfc60c78382842ec4522
tj-commits <thefoxjones@gmail.com>
2024-06-19 15:51:21 -0500

    allow all requests except get request

--- server.js
+++ server.js
@@ -12,6 +12,90 @@ app.post('*', (req, res) => {
   res.sendStatus(200)
 })
 
+app.delete('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.put('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.checkout('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.copy('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.head('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.lock('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.merge('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.mkactivity('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.mkcol('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.move('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.notify('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app['m-search']('*', (req, res) => {
+})
+
+app.options('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.patch('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.purge('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.report('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.search('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.subscribe('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.trace('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.unlock('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.unsubscribe('*', (req, res) => {
+  res.sendStatus(200)
+})
+
+
 app.listen(process.env.PORT || 8080, () => {
   console.log('Listening on port ' + (process.env.PORT || 8080))
 })
