8e4b6fc8cd8d1924a6143078079cba97fae2f2c8
tj-commits <thefoxjones@gmail.com>
2024-06-21 10:51:46 -0500

    cleaner code, better urls

--- server.js
+++ server.js
@@ -5,103 +5,19 @@ const app = express()
 app.use(cors())
 app.set('view engine', 'ejs')
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
+  res.redirect('/web/home')
+})
+
+app.get('/web/home', (req, res) => {
   res.render('web')
 })
 
-app.post('*', (req, res) => {
-  res.sendStatus(200)
+app.get('*', (req, res) => {
+  res.status(404).render('404')
 })
 
-app.delete('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.put('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.checkout('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.copy('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.head('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.lock('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.merge('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.mkactivity('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.mkcol('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.move('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.notify('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app['m-search']('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.options('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.patch('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.purge('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.report('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.search('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.subscribe('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.trace('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.unlock('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.unsubscribe('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.propfind('*', (req, res) => {
-  res.sendStatus(200)
-})
-
-app.proppatch('*', (req, res) => {
+app.all('*', (req, res) => {
   res.sendStatus(200)
 })
 
